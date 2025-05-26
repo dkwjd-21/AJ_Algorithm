09d01ca734fbce2a1ee3fdb2c50ed0526568a43f
dkwjd-21 <137870180+dkwjd-21@users.noreply.github.com>
2025-05-27 01:46:01 +0900

    [Silver V] Title: 색종이, Time: 108 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,42 @@
+import java.io.*;
+
+public class Main {	
+	public static void main(String[] args) throws IOException  {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		// 색종이의 수
+		int n = Integer.parseInt(br.readLine());
+		
+		// 100*100 도화지
+		boolean[][] paper = new boolean[100][100];
+		
+		// 색종이의 수만큼 반복문으로 색종이의 왼쪽 아래 점 좌표 입력받음
+		for(int i=0; i<n; i++) {
+			String[] input = br.readLine().split(" ");
+			int x = Integer.parseInt(input[0]);
+			int y = Integer.parseInt(input[1]);
+			
+			// [x][y]~[x+10][y+10] 까지 true로 바꾸기 
+			// x와 y는 도화지의 왼쪽&아랫쪽 변 사이의 거리이기 때문에 0부터 시작함
+			// = 배열의 인덱스와 일치함 
+			for(int j=x; j<x+10; j++) {
+				for(int k=y; k<y+10; k++) {
+					paper[j][k] = true;
+				}
+			}
+		}
+		
+		// true 개수 = 넓이
+		int cnt = 0;
+		for(int i=0; i<paper.length; i++) {
+			for(int j=0; j<paper[i].length; j++) {
+				if(paper[i][j] == true) {
+					cnt++;
+				}
+			}
+		}
+		
+		// 넓이 출력
+		System.out.println(cnt);
+	}
+}
