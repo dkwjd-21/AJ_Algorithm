6f76714f6e8f87b0aa16c3be653b7f1ca83514b6
송아정 <137870180+dkwjd-21@users.noreply.github.com>
2025-06-08 23:52:50 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 100 ms, Memory: 14092 KB -BaekjoonHub

---
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		/*	규칙
+		 * 첫줄의 정사각형의 개수는 2^n
+		 * 첫줄의 점의 개수는 2^n+1
+		 * 전체 점의 개수는 2^n+1의 제곱
+		 * */
+		
+		// N이 주어진다
+		int N = Integer.parseInt(br.readLine());
+		System.out.println(countDot(N));
+		
+		br.close();
+	}
+	
+	public static int countDot(int N) {
+		double cnt = 0.0;
+		
+		cnt = Math.pow(Math.pow(2, N)+1, 2);
+		
+		return (int)cnt;
+	}
+}
