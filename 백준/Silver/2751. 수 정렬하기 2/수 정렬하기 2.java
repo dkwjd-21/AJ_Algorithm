8dc2efb72a5fa5fa44921c75fac11c83571a3eff
송아정 <137870180+dkwjd-21@users.noreply.github.com>
2025-06-30 18:02:32 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1424 ms, Memory: 137308 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -3,7 +3,9 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
@@ -12,15 +14,20 @@ public class Main {
 		
 		// 수의 개수  N <= 1,000,000  
 		int N = Integer.parseInt(br.readLine());
-		int list[] = new int[N];
+		List<Integer> list = new ArrayList<>();
+//		int list[] = new int[N];
 		
 		// O(n)
 		for(int i=0; i<N; i++) {
-			list[i] = Integer.parseInt(br.readLine());
+//			list[i] = Integer.parseInt(br.readLine());
+			list.add(Integer.parseInt(br.readLine()));
 		}
 		
-		// TimSort 방식. O(NlogN)
-		Arrays.sort(list);
+		// O(NlogN) - 최악 O(N^2)
+		// Arrays.sort(list);
+		
+		// 평균&최악 O(NlogN)
+		Collections.sort(list);
 		
 		// O(n) 
 		for(int i : list) {
