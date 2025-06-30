0a95dea1b9da59a8b1772a64916dc7eb116644ac
송아정 <137870180+dkwjd-21@users.noreply.github.com>
2025-07-01 01:00:41 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 412 ms, Memory: 21080 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i=1; i<=2*N-1; i++) {
+			if(i<=N) {
+				for(int j=1; j<i; j++) {
+					System.out.print(" ");
+				}				
+				for(int j=1; j<=2*(N-i)+1; j++) {
+					System.out.print("*");
+				}
+			} else {
+				for(int j=2*N-i-1; j>0; j--) {
+					System.out.print(" ");
+				}
+				for(int j=0; j<2*(i-N)+1; j++) {
+					System.out.print("*");
+				}
+			}			
+			System.out.println();
+		}
+		
+		br.close();
+	}
+}
