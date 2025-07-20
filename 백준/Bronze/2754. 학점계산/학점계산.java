006fb54b786d937dfb5316a81f637c22cfa13086
송아정 <137870180+dkwjd-21@users.noreply.github.com>
2025-07-21 04:11:56 +0900

    [Bronze III] Title: 학점계산, Time: 116 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		// C언어 성적
+		String score = br.readLine();
+		
+		// 성적별 map 
+		Map<String, Double> map = new HashMap<>();
+		map.put("A+", 4.3);
+		map.put("A0", 4.0);
+		map.put("A-", 3.7);
+		map.put("B+", 3.3);
+		map.put("B0", 3.0);
+		map.put("B-", 2.7);
+		map.put("C+", 2.3);
+		map.put("C0", 2.0);
+		map.put("C-", 1.7);
+		map.put("D+", 1.3);
+		map.put("D0", 1.0);
+		map.put("D-", 0.7);
+		map.put("F", 0.0);
+		
+		bw.append(map.get(score)+"");
+		
+		bw.close();
+		br.close();
+	}
+	
+}
