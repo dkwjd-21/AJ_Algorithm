bc55c3f8c6da47e33e00897b3e00479c7510bd29
송아정 <137870180+dkwjd-21@users.noreply.github.com>
2026-01-31 21:23:49 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length];
+        
+        int idx = num_list.length - 1;
+        for(int num : num_list){
+            answer[idx] = num;
+            idx--;
+        }
+        
+        return answer;
+    }
+}
