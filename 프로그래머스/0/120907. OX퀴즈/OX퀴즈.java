932fd4a62e8ec03c27665164868017bb4e64b107
송아정 <137870180+dkwjd-21@users.noreply.github.com>
2026-03-03 23:26:45 +0900

    [level 0] Title: OX퀴즈, Time: 0.24 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String[] solution(String[] quiz) {
+        String[] answer = new String[quiz.length];
+        
+        for(int i=0; i<quiz.length; i++) {
+            String[] tmp = quiz[i].split(" ");
+            
+            if(tmp[1].equals("+")) {
+                if(Integer.parseInt(tmp[0]) + Integer.parseInt(tmp[2]) == Integer.parseInt(tmp[4])) {
+                    answer[i] = "O";
+                } else {
+                    answer[i] = "X";
+                }
+            } else if(tmp[1].equals("-")) {
+                if(Integer.parseInt(tmp[0]) - Integer.parseInt(tmp[2]) == Integer.parseInt(tmp[4])) {
+                    answer[i] = "O";
+                } else {
+                    answer[i] = "X";
+                }
+            }
+            
+        }
+        
+        return answer;
+    }
+}
