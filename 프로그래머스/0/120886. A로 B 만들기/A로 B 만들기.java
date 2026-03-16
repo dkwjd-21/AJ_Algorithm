79dbfb332de42bef8cc6e6bbfa1eeb01880919bf
송아정 <137870180+dkwjd-21@users.noreply.github.com>
2026-03-16 15:35:18 +0900

    Update A로 B 만들기.java

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
@@ -1,9 +1,11 @@
 class Solution {
     public int solution(String before, String after) {
         // before의 순서를 바꾸어 after를 만들 수 있으면 1, 없으면 0 리턴
-        int answer = 0;
+        int answer;
         
-        for(int i=0; i<after.length(); i++) {
+        /*
+            // 첫번째 시도 (평균 5초 정도 소요)
+             for(int i=0; i<after.length(); i++) {
             String tmp = after.charAt(i) + "";
             // replaceFirst : 처음 나오는 음절만 대체
             before = before.replaceFirst(tmp, "");
@@ -11,8 +13,26 @@ class Solution {
         
         if(before.isEmpty()) {
             answer = 1;
-        } 
+        } else {
+            answer = 0;
+        }
         
-        return answer;
+        */
+        
+        // 성능 개선 버전 - 평균 0.5초 소요
+        int[] alphabet = new int[26];
+        
+        for(int i=0; i<before.length(); i++) {
+            alphabet[before.charAt(i) - 'a']++;
+            alphabet[after.charAt(i) - 'a']--;
+        }
+       
+        for(int i=0; i<26; i++) {
+            if(alphabet[i] != 0) {
+                return answer = 0;
+            }
+        }
+        
+        return answer = 1;
     }
 }
