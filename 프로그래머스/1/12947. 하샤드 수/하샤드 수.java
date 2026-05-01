d697588a0a8ead88b06c5ed95af0af7c4f583d02
송아정 <137870180+dkwjd-21@users.noreply.github.com>
2026-05-01 23:34:35 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 91.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        
+        int sum = 0;
+        int tmp = x;
+        while(tmp != 0) {
+            sum = sum + tmp%10;
+            tmp = tmp/10;
+        }
+        
+        answer = (x%sum == 0);
+            
+        return answer;
+    }
+}
